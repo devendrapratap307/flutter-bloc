23f8deced761524c9621d6c2c32b30aa3bd91035
Pratap <pratapdevendra307@gmail.com>
2025-05-11 19:05:27 +0530

    Erro and actual data checck

--- lib/login/bloc/login_bloc.dart
+++ lib/login/bloc/login_bloc.dart
@@ -1,17 +1,22 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:store/login/bloc/login_event.dart';
 import 'package:store/login/bloc/login_state.dart';
+import 'package:store/login/model/error_model.dart';
 import 'package:store/login/repo/login_repo.dart';
 
 import '../model/login_resp_model.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginState>{
   LoginBloc(): super(LoginInitialState()){ //initial State
-    on<LoginReqEvent>((event, emit) async{//initial event
+    on<LoginReqEvent>((event, emit) async{
       emit(LoginLoadingState());
       try {
-        LoginRespModel respModel = await LoginRepo().login(event.loginReqModel?.username??"", event.loginReqModel?.password??"");
-        emit(LoginLoadedState(loginRespModel: respModel));
+        var respModel = await LoginRepo().login(event.loginReqModel?.username??"", event.loginReqModel?.password??"");
+        if(respModel is LoginRespModel){
+          emit(LoginLoadedState(loginRespModel: respModel));
+        } else if (respModel is ErrorRespModel){
+          emit(LoginErrorState(errorMsg: respModel.message?.toString()));
+        }
       } catch (e){
         emit(LoginErrorState(errorMsg: e.toString()));
       }
--- lib/login/repo/login_repo.dart
+++ lib/login/repo/login_repo.dart
@@ -1,22 +1,23 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:store/login/model/error_model.dart';
 import 'package:store/login/model/login_req_model.dart';
 import 'package:store/login/model/login_resp_model.dart';
 class LoginRepo {
-  Future<LoginRespModel> login(String username, String password) async{
+  Future<Object> login(String username, String password) async{ // Object ===> LoginRespModel or ErrorRespModel
     String url ='https://dummyjson.com/user/login';
     LoginReqModel loginReqModel = LoginReqModel(username:username, password:password);
     http.Response response = await http.post(Uri.parse(url),
         headers: { 'Content-Type': 'application/json' },
         body: jsonEncode(loginReqModel.toJson()));
     try{
+      var resp = response.body;
+      var respBody = jsonDecode(resp);
       if(response.statusCode==200){
-        var resp = response.body;
-        var respBody = jsonDecode(resp);
         return LoginRespModel.fromJson(respBody);
       } else {
-        return LoginRespModel();
+        return ErrorRespModel.fromJson(respBody);
       }
     } catch(e){
       throw Exception(e);
